70f0c162a4cb8d9490efee11d62795b434193dfc
Tizi006 <Tiz.Zimmermann@web.de>
2023-05-23 22:25:38 +0200

    added table to station

--- app.js
+++ app.js
@@ -20,16 +20,23 @@ app.use(express.static("public"));
 app.set("views", "views");
 app.set("view engine", "pug");
 
-app.get("/", function(request, response) {
+app.get("/", function (request, response) {
     response.render("Login");
 });
-app.get("/dashboard", function(request, response) {
+app.get("/dashboard", function (request, response) {
     response.render("Dashboard");
 });
-app.get("/stations/1", function(request, response) {
-    response.render("Station.pug");
+app.get("/stations/1", function (request, response) {
+    let data = [{wetter: "800", temp: "20.32", wind: "0.89", pressure: "1016"},
+        {wetter: "800", temp: "20.32", wind: "0.89", pressure: "1016"},
+        {wetter: "400", temp: "32.5", wind: "15", pressure: "90"},
+        {wetter: "400", temp: "32.5", wind: "15", pressure: "90"},
+        {wetter: "800", temp: "11.16", wind: "2.23", pressure: "1024"},
+        {wetter: "800", temp: "11.16", wind: "2.23", pressure: "1024"},
+        {wetter: "800", temp: "-6.14", wind: "1.15", pressure: "1027"}]
+    response.render("Station.pug", {data: data});
 });
 
-app.listen(PORT, function() {
-  console.log(`Weathertop running and listening on port ${PORT}`);
+app.listen(PORT, function () {
+    console.log(`Weathertop running and listening on port ${PORT}`);
 });
