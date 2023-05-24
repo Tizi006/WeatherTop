5931c724244f1b4e6798e5cb3be85cacd431463a
Tizi006 <Tiz.Zimmermann@web.de>
2023-05-24 20:51:29 +0200

    Revert "added a background, animations finished for release 0"
    This reverts commit f2b0642505eb40b5b2e85ab335153f8214476fe5.

--- public/scripts/changeDisplayedInfo.js
+++ public/scripts/changeDisplayedInfo.js
@@ -36,8 +36,4 @@ cards.forEach((card) => {
             }
         });
     }
-    //handles animation
-    cards.forEach((card, index) => {
-        card.style.animationDelay = `${index * 0.4}s`;
-    });
 });
