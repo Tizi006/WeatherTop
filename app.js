124120f23937fd66ece40639094ead85701c3a70
Tizi006 <Tiz.Zimmermann@web.de>
2023-05-31 09:40:04 +0200

    added login form

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const {urlencoded} = require("express");
 const pg = require("pg");
 const {data} = require("express-session/session/cookie");
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 /* Reading global variables from config file */
@@ -29,17 +31,69 @@ app.set("view engine", "pug");
 app.get("/", function (request, response) {
     response.render("Login");
 });
-app.get("/dashboard", function (request, response) {
-    dbClient.query("select * from stations s join weatherdata w on s.id =w.station_id WHERE w.id IN (SELECT MIN(id) FROM weatherdata GROUP BY station_id);", function (dbError, dbResponse) {
-            response.render("Dashboard", {data: dbResponse.rows});
-    })
+app.get("/dashboard", renderDashboard)
+app.post("/dashboard", urlencodedParser, function (request, response) {
+    const titel = request.params.titel;
+    const lat = request.params.lat;
+    const lon = request.params.lon;
+    //  if(titel!=null&&lat!=null&&lon!=null){
+    dbClient.query("insert into stations (name, lon, lat,user_id) values ('bob',49.99,40.00,1)")
+    //  }
+    renderDashboard(request, response);
 });
-app.get("/stations/:id", function (request, response) {
-    dbClient.query("select * from stations s join weatherdata w on s.id =w.station_id where station_id =$1;", [request.params.id], function (dbError, dbResponse) {
-        if(dbResponse.rows.length===0){
-            response.render("error",{text:"Ups! Station nicht gefunden"});
+
+function renderDashboard(request, response) {
+    let stations, values;
+
+    dbClient.query(
+        "SELECT name, lon, lat FROM stations WHERE user_id = 1",
+        function (dbError, dbResponse) {
+            if (dbError) {
+                console.error(dbError);
+                return;
+            }
+
+            stations = dbResponse.rows;
+
+            dbClient.query(
+                "SELECT w.station_id, weathercode, temperature, wind, pressure FROM stations s LEFT JOIN weatherdata w ON s.id = w.station_id WHERE w.id IN (SELECT MIN(id) FROM weatherdata GROUP BY station_id)",
+                function (dbError, dbResponse) {
+                    if (dbError) {
+                        console.error(dbError);
+                        return;
+                    }
+
+                    values = dbResponse.rows;
+                    const data = mergeArrays(stations, values);
+                    response.render("Dashboard", { data: data });
+                }
+            );
         }
-        else {
+    );
+}
+
+function mergeArrays(stations, values) {
+    const mergedData = [];
+
+    for (const station of stations) {
+        const matchingValue = values.find((value) => value.station_id === station.id);
+        const data = {
+            ...station,
+            ...(matchingValue || { weathercode: 0, temperature: 0, wind: 0, pressure: 0 }),
+        };
+        mergedData.push(data);
+    }
+
+    return mergedData;
+}
+
+
+
+app.get("/stations/:id", function (request, response) {
+    dbClient.query("select * from stations s join weatherdata w on s.id =w.station_id where station_id =$1 order by time desc", [request.params.id], function (dbError, dbResponse) {
+        if (dbResponse.rows.length === 0) {
+            response.render("error", {text: "Ups! Station nicht gefunden"});
+        } else {
             response.render("Station", {data: dbResponse.rows});
         }
     })
--- public/scripts/changeDisplayedInfo.js
+++ public/scripts/changeDisplayedInfo.js
@@ -1,49 +1,50 @@
-const cards =(document.querySelectorAll('.card'));
+const cards = (document.querySelectorAll('.card'));
 cards.forEach((card, index) => {
-    const buttons = card.querySelectorAll('.displayed-selector');
-    const dataElements = card.querySelectorAll('.card-data');
+        const buttons = card.querySelectorAll('.displayed-selector');
+        const dataElements = card.querySelectorAll('.card-data');
 
-    // Set initial display to "none" for windview and pressureview
-    const windview = card.querySelector('.wind-data');
-    const pressureview = card.querySelector('.pressure-data');
-    windview.style.display = 'none';
-    pressureview.style.display = 'none';
-
-    buttons.forEach((button) => {
-        if (button.dataset.target === 'temp') {
-            button.classList.add('active');
-        }
-    });
-
-    card.addEventListener('click', (event) => {
-        const targetButton = event.target.closest('.displayed-selector');
-        if (!targetButton) return;
-
-        const target = targetButton.dataset.target;
+        // Set initial display to "none" for windview and pressureview
+        const windview = card.querySelector('.wind-data');
+        const pressureview = card.querySelector('.pressure-data');
+        windview.style.display = 'none';
+        pressureview.style.display = 'none';
 
         buttons.forEach((button) => {
-            if (button.dataset.target === target) {
+            if (button.dataset.target === 'temp') {
                 button.classList.add('active');
-            } else {
-                button.classList.remove('active');
             }
         });
 
-        dataElements.forEach((dataElement) => {
-            if (dataElement.classList.contains(`${target}-data`)) {
-                dataElement.style.display = 'flex';
-            } else {
-                dataElement.style.display = 'none';
-            }
+        card.addEventListener('click', (event) => {
+            const targetButton = event.target.closest('.displayed-selector');
+            if (!targetButton) return;
+
+            const target = targetButton.dataset.target;
+
+            buttons.forEach((button) => {
+                if (button.dataset.target === target) {
+                    button.classList.add('active');
+                } else {
+                    button.classList.remove('active');
+                }
+            });
+
+            dataElements.forEach((dataElement) => {
+                if (dataElement.classList.contains(`${target}-data`)) {
+                    dataElement.style.display = 'flex';
+                } else {
+                    dataElement.style.display = 'none';
+                }
+            });
         });
-    });
 
-    // Handles animation
-    card.style.animationDelay = `${index * 0.4}s`;
-
-    // Handles background
-    let img;
-    const weathercode=  card.querySelector('.weather-code').textContent;
+        // Handles animation
+        card.style.animationDelay = `${index * 0.4}s`;
+        const newcard = (document.querySelector('#new-card'));
+        newcard.style.animationDelay = `${(cards.length-1) * 0.4}s`
+        // Handles background
+        let img;
+        const weathercode = card.querySelector('.weather-code').textContent;
         switch (weathercode) {
             case "200": //Thunderstorm
                 img = "../images/thunderstrom.jpg";
